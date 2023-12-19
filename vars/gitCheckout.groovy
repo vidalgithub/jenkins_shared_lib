fbbac21681cc3716f96026ce283b31f6c5f5087f
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2023-12-18 22:56:31 -0600

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,8 @@
 def call(Map stageParams) {
-    checkout (branches: [[name: '${stageParams.branch}']], extensions: [], userRemoteConfigs: [[url: '${stageParams.url}']])
-    // checkout([
-    //     $class: 'GitSCM',
-    //     branches: [[name:  stageParams.branch ]],
-    //     userRemoteConfigs: [[ url: stageParams.url ]]
-    // ])
+    
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
   }
